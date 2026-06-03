26ff55d10e2f03fd111c180b4c888c8aa5b28566
halmos <80233769+MyWolve@users.noreply.github.com>
2026-06-03 16:16:34 -0400

    Naive PWM control implemented through SPEED command and applySpeed() func. Intuitive sliding scale tbi when web-app completed.

--- controls/controls.ino
+++ controls/controls.ino
@@ -1,36 +1,59 @@
-const int IN1 = 2;  // L298N-F input 1
-const int IN2 = 3;  // L298N-F input 2
-const int IN3 = 4;  // L298N-F input 3
-const int IN4 = 5;  // L298N-F input 4
+const int F_IN1 = 2;  // L298N-F input 1
+const int F_IN2 = 4;  // L298N-F input 2
+const int F_IN3 = 7;  // L298N-F input 3
+const int F_IN4 = 8;  // L298N-F input 4
 
-const int INA = 8;  // L298N-R input A
-const int INB = 9;  // L298N-R input B
-const int INC = 10; // L298N-R input C
-const int IND = 11; // L298N-R input D
+const int F_ENA = 3;  // L298N-F PWM (F1,F2)
+const int F_ENB = 5;  // L298N-F PWM (F3,F4)
+
+const int R_IN1 = 12;  // L298N-R input A
+const int R_IN2 = 13;  // L298N-R input B
+const int R_IN3 = A0;  // L298N-R input C
+const int R_IN4 = A1;  // L298N-R input D
+
+const int R_ENA = 9;   // L298N-R PWM (R1,R2)
+const int R_ENB = 10;  // L298N-R PWM (R3,R4)
 
 enum Direction { DIR_STOP, DIR_FWD, DIR_REV }; // Current state of droid
-Direction currentDir = DIR_STOP;               // Begins motionless
+// Begins motionless
+Direction currentDir = DIR_STOP;               
+int currentSpeed = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(INA, OUTPUT);
-  pinMode(INB, OUTPUT);
-  pinMode(INC, OUTPUT);
-  pinMode(IND, OUTPUT);
+
+  // Initialize motor pins
+  pinMode(F_IN1, OUTPUT);
+  pinMode(F_IN2, OUTPUT);
+  pinMode(F_IN3, OUTPUT);
+  pinMode(F_IN4, OUTPUT);
+  pinMode(R_IN1, OUTPUT);
+  pinMode(R_IN2, OUTPUT);
+  pinMode(R_IN3, OUTPUT);
+  pinMode(R_IN4, OUTPUT);
+
+  // Initialize PWM pins
+  pinMode(F_ENA, OUTPUT);
+  pinMode(F_ENB, OUTPUT);
+  pinMode(R_ENA, OUTPUT);
+  pinMode(R_ENB, OUTPUT);
 
   // Start with motor off
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, LOW);
-  digitalWrite(INA, LOW);
-  digitalWrite(INB, LOW);
-  digitalWrite(INC, LOW);
-  digitalWrite(IND, LOW);
+  digitalWrite(F_IN1, LOW);
+  digitalWrite(F_IN2, LOW);
+  digitalWrite(F_IN3, LOW);
+  digitalWrite(F_IN4, LOW);
+  digitalWrite(R_IN1, LOW);
+  digitalWrite(R_IN2, LOW);
+  digitalWrite(R_IN3, LOW);
+  digitalWrite(R_IN4, LOW);
+
+  // Start with PWM pins at default speed
+  analogWrite(F_ENA, currentSpeed);
+  analogWrite(F_ENB, currentSpeed);
+  analogWrite(R_ENA, currentSpeed);
+  analogWrite(R_ENB, currentSpeed);
+  
   Serial.println("Ready. Commands: '2 HIGH', '2 LOW', '4 HIGH', '4 LOW', ... 'STOP'");
 }
 void loop() {
@@ -39,25 +62,32 @@ void loop() {
     cmd.trim();
     cmd.toUpperCase();
     // Manual pin commands sidestep state setup, BE AWARE
-    if      (cmd == "2 HIGH") { digitalWrite(IN1, HIGH); Serial.println("Pin 2 HIGH");  }
-    else if (cmd == "2 LOW")  { digitalWrite(IN1, LOW);  Serial.println("Pin 2 LOW");   }
-    else if (cmd == "3 HIGH") { digitalWrite(IN2, HIGH); Serial.println("Pin 3 HIGH");  }
-    else if (cmd == "3 LOW")  { digitalWrite(IN2, LOW);  Serial.println("Pin 3 LOW");   }
-    else if (cmd == "4 HIGH") { digitalWrite(IN3, HIGH);  Serial.println("Pin 4 HIGH"); }
-    else if (cmd == "4 LOW")  { digitalWrite(IN3, LOW);  Serial.println("Pin 4 LOW");   }
-    else if (cmd == "5 HIGH") { digitalWrite(IN4, HIGH);  Serial.println("Pin 5 HIGH"); }
-    else if (cmd == "5 LOW")  { digitalWrite(IN4, LOW);  Serial.println("Pin 5 LOW");   }
-    else if (cmd == "8 HIGH") { digitalWrite(INA, HIGH);  Serial.println("Pin 8 HIGH"); }
-    else if (cmd == "8 LOW")  { digitalWrite(INA, LOW);  Serial.println("Pin 8 LOW");   }
-    else if (cmd == "9 HIGH") { digitalWrite(INB, HIGH);  Serial.println("Pin 9 HIGH"); }
-    else if (cmd == "9 LOW")  { digitalWrite(INB, LOW);  Serial.println("Pin 9 LOW");   }
-    else if (cmd == "10 HIGH"){ digitalWrite(INC, HIGH);  Serial.println("Pin 10 HIGH");}
-    else if (cmd == "10 LOW") { digitalWrite(INC, LOW);  Serial.println("Pin 10 LOW");  }
-    else if (cmd == "11 HIGH"){ digitalWrite(IND, HIGH);  Serial.println("Pin 11 HIGH");}
-    else if (cmd == "11 LOW") { digitalWrite(IND, LOW);  Serial.println("Pin 11 LOW");  }
+    if      (cmd == "2 HIGH") { digitalWrite(F_IN1, HIGH); Serial.println("Pin 2 HIGH");  }
+    else if (cmd == "2 LOW")  { digitalWrite(F_IN1, LOW);  Serial.println("Pin 2 LOW");   }
+    else if (cmd == "3 HIGH") { digitalWrite(F_IN2, HIGH); Serial.println("Pin 3 HIGH");  }
+    else if (cmd == "3 LOW")  { digitalWrite(F_IN2, LOW);  Serial.println("Pin 3 LOW");   }
+    else if (cmd == "4 HIGH") { digitalWrite(F_IN3, HIGH);  Serial.println("Pin 4 HIGH"); }
+    else if (cmd == "4 LOW")  { digitalWrite(F_IN3, LOW);  Serial.println("Pin 4 LOW");   }
+    else if (cmd == "5 HIGH") { digitalWrite(F_IN4, HIGH);  Serial.println("Pin 5 HIGH"); }
+    else if (cmd == "5 LOW")  { digitalWrite(F_IN4, LOW);  Serial.println("Pin 5 LOW");   }
+    else if (cmd == "8 HIGH") { digitalWrite(R_IN1, HIGH);  Serial.println("Pin 8 HIGH"); }
+    else if (cmd == "8 LOW")  { digitalWrite(R_IN1, LOW);  Serial.println("Pin 8 LOW");   }
+    else if (cmd == "9 HIGH") { digitalWrite(R_IN2, HIGH);  Serial.println("Pin 9 HIGH"); }
+    else if (cmd == "9 LOW")  { digitalWrite(R_IN2, LOW);  Serial.println("Pin 9 LOW");   }
+    else if (cmd == "10 HIGH"){ digitalWrite(R_IN3, HIGH);  Serial.println("Pin 10 HIGH");}
+    else if (cmd == "10 LOW") { digitalWrite(R_IN3, LOW);  Serial.println("Pin 10 LOW");  }
+    else if (cmd == "11 HIGH"){ digitalWrite(R_IN4, HIGH);  Serial.println("Pin 11 HIGH");}
+    else if (cmd == "11 LOW") { digitalWrite(R_IN4, LOW);  Serial.println("Pin 11 LOW");  }
     
     else if (cmd == "FWD")    { FWD(); Serial.println("Pin 2,4,8,10 HIGH"); }
     else if (cmd == "REV")    { REV(); Serial.println("Pin 3,5,9,11 HIGH"); }
+    else if (cmd.startsWith("SPEED ")) {
+      int newSpeed = cmd.substring(6).toInt();
+      newSpeed = constrain(newSpeed, 0, 255); // Motors don't actually work between [0,255], it's more like [100,255]. This is due to the motors not being able to interpret switches in voltage above a certain frequency
+      currentSpeed = newSpeed;
+      Serial.print("Speed set to "); Serial.println(currentSpeed);
+      applySpeed(); // Apply newSpeed
+      }
     else if (cmd == "STOP")   {
       STOP();
       Serial.println("ALL LOW (motor stopped)");
@@ -70,14 +100,14 @@ void STOP(){
     // Set currentDir to STOP
     currentDir = DIR_STOP;
     // Set wheels to LOW position
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, LOW);
-    digitalWrite(INA, LOW);
-    digitalWrite(INB, LOW);
-    digitalWrite(INC, LOW);
-    digitalWrite(IND, LOW);
+    digitalWrite(F_IN1, LOW);
+    digitalWrite(F_IN2, LOW);
+    digitalWrite(F_IN3, LOW);
+    digitalWrite(F_IN4, LOW);
+    digitalWrite(R_IN1, LOW);
+    digitalWrite(R_IN2, LOW);
+    digitalWrite(R_IN3, LOW);
+    digitalWrite(R_IN4, LOW);
   }
   
 
@@ -90,23 +120,23 @@ void FWD(){
     STOP();
     delay(200);
     // Set wheels to FWD
-    digitalWrite(IN1, HIGH);
+    digitalWrite(F_IN1, HIGH);
     //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-    digitalWrite(IN3, HIGH);
+    digitalWrite(F_IN3, HIGH);
     //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-    digitalWrite(INA, HIGH);
+    digitalWrite(R_IN1, HIGH);
     //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-    digitalWrite(INC, HIGH);
+    digitalWrite(R_IN3, HIGH);
     currentDir = DIR_FWD;
   }
   // Set wheels to FWD
-  digitalWrite(IN1, HIGH);
+  digitalWrite(F_IN1, HIGH);
   //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-  digitalWrite(IN3, HIGH);
+  digitalWrite(F_IN3, HIGH);
   //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-  digitalWrite(INA, HIGH);
+  digitalWrite(R_IN1, HIGH);
   //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-  digitalWrite(INC, HIGH);
+  digitalWrite(R_IN3, HIGH);
   currentDir = DIR_FWD;
 }
 
@@ -119,22 +149,29 @@ void REV(){
     STOP();
     delay(200);
     // Set wheels to REV
-    digitalWrite(IN2, HIGH);
+    digitalWrite(F_IN2, HIGH);
     //delay(50); // TEMPORARY: REMOVE AFTER TESTING
-    digitalWrite(IN4, HIGH);
+    digitalWrite(F_IN4, HIGH);
     //delay(50); // TEMPORARY: REMOVE AFTER TESTING
-    digitalWrite(INB, HIGH);
+    digitalWrite(R_IN2, HIGH);
     //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-    digitalWrite(IND, HIGH);
+    digitalWrite(R_IN4, HIGH);
     currentDir = DIR_REV;
   }
   // Set wheels to REV
-  digitalWrite(IN2, HIGH);
+  digitalWrite(F_IN2, HIGH);
   //delay(50); // TEMPORARY: REMOVE AFTER TESTING
-  digitalWrite(IN4, HIGH);
+  digitalWrite(F_IN4, HIGH);
   //delay(50); // TEMPORARY: REMOVE AFTER TESTING
-  digitalWrite(INB, HIGH);
+  digitalWrite(R_IN2, HIGH);
   //delay(50);// TEMPORARY: REMOVE AFTER TESTING
-  digitalWrite(IND, HIGH);
+  digitalWrite(R_IN4, HIGH);
   currentDir = DIR_REV;
+}
+
+void applySpeed(){
+  analogWrite(F_ENA, currentSpeed);
+  analogWrite(F_ENB, currentSpeed);
+  analogWrite(R_ENA, currentSpeed);
+  analogWrite(R_ENB, currentSpeed);
 }
